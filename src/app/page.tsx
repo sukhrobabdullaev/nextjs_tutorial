25606e9631d7137e2f4bf2a8a3b22e1fd5e459de
Sukhrob Abdullaev <s.abdullaev@tuit.uz>
2024-12-02 14:52:50 +0900

    lesson4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-'use client'
-
-import Image from "next/image"
-import { useState } from 'react'
-
 export default function Home() {
-  const [counter, setCounter] = useState(0)
-
-  return (
-    <div>
-      <h1>Hello world!</h1>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter(prev => prev + 1)}>Click</button>
-    </div>
-  )
+  return <h1>Home Page</h1>
 }
